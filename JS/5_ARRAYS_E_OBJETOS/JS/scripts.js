fe866d72752ec7c0eb46bd52771e63e682da09e0
lorenzo <lorenzostrin@gmail.com>
2025-05-19 16:13:09 -0300

    atualização

--- JS/5_ARRAYS_E_OBJETOS/JS/scripts.js
+++ JS/5_ARRAYS_E_OBJETOS/JS/scripts.js
@@ -49,63 +49,222 @@
 
 // console.log(typeof personagem)
 
-// 6 - Criando e deletando dados
-const carro = {
-    motor: 2.0,
-    marca: "VW",
-    modelo: "Tiguan",
-    km: 20000,
+// // 6 - Criando e deletando dados
+// const carro = {
+//     motor: 2.0,
+//     marca: "VW",
+//     modelo: "Tiguan",
+//     km: 20000,
+// }
+
+// console.log(carro)
+
+// carro.porta = 4 // adicionado uma nova propriedade
+
+// console.log(carro)
+
+// delete carro.km // deletando uma propriedade
+
+// console.log(carro)
+
+// // 
+// const obj = {
+//     a: "teste",
+//     b: true,
+// }
+
+// console.log(obj instanceof Object) //instanceof  verifica se um objeto é filho de uma classe
+
+// const obj2 = {
+//     c: [],
+// }
+
+// Object.assign(obj2, obj)
+
+// console.log(obj2)
+
+// // 8 - Conhecendo melhor os objetos
+// console.log(Object.keys(obj)) //keys: verifica as propriedades de um objeto
+// console.log(Object.keys(obj2))
+
+// console.log(Object.entries(obj)) //entries: recebemos arrays dos nomes das propriedades com seus valores
+
+// // 9 - mutação
+// //quando criamos um objeto a partir de outro, este novo objeto não é novo e sim uma referência do primeiro
+// const a = {
+//     name: "lorenzo"
+// }
+
+// const b = a //adicionado a em b, b se torna uma referência de a e vice versa
+
+// console.log(a) 
+// console.log(b) //Adicionou o "lorenzo"
+
+// a.ano = 19 //adicionando ano ao a
+
+// console.log(a)
+// console.log(b) //Vemos que também adicionou no b
+
+// delete b.ano //deletando ano do b
+
+// console.log(a) //vemos que também deletou do a
+// console.log(b)
+
+// // 10 - loop em array
+// const user = ["Lorenzo", "Lucas", "Pedro", "José"]
+
+// for(let i = 0; i < user.length; i++){ //user.length vai ver quantos elementos o user tem
+//     console.log(`Listando o usuário: ${user[i]}`) //acessa o item pelo contador i, assim acompanhando o número
+// }
+
+// // 11 - Push e pop
+// const array = ["a", "b", "c"]
+
+// array.push("d") //.push adiciona um elemento no final do array, pode adicionar varios elementos
+
+// console.log(array)
+
+// console.log(array.length)
+
+// array.pop() //.pop remove um elemento do final do array
+
+// console.log(array)
+
+// const itemRemovido = array.pop() //O pop fica guardado, assim conseguindo adicionar em uma variavel
+
+// console.log(itemRemovido)
+
+// console.log(array)
+
+// // 12 - shift e unshift
+// const lists = ["a", "b", "c"]
+
+// const list = lists.shift() //Shift remove o primeiro elemento do array
+
+// console.log(list) //Mostra o que foi removido
+
+// console.log(lists) //mostra o array sem o primeiro elemento
+
+// lists.unshift("1", "z") //unshift Adiciona elementos na frente do array
+
+// console.log(lists)
+
+// //  13 - indexOf e lastIndexOf
+// const frutas = ["morango", "uva", "maça", "uva"]
+
+// console.log(frutas.indexOf("uva")) // indexOf diz em qual posição o elemento está
+// console.log(frutas.indexOf("maça"))
+
+// console.log(frutas.lastIndexOf("uva")) // lastIndexOf mostra o ultimo do elemento
+
+// // 14 - slice
+// const testeSlice = ["a", "b", "c", "d", "e", "f"]
+
+// const subMatriz = testeSlice.slice(2, 4) //slice vai mostrar os elementos de x até x - 2 até 4
+
+// console.log(subMatriz) //Mas há um problema ele não mostra o ultimo, (2, 4)exemplo("c", "d")
+
+// console.log(testeSlice)
+
+// const subMatriz2 = testeSlice.slice(2, 4 + 1) //Para mostrar o ultimo elemento precisa adicionar o +1
+
+// console.log(subMatriz2)
+
+// const subMatriz3 = testeSlice.slice(2)//só colocando 1 valor, ele vai mostrar os seguintes
+
+// console.log(subMatriz3)
+
+// // 15 - foreach
+// const nums = [1, 2, 3, 4, 5]
+
+// nums.forEach((numero) => { //O foreach é como o for ou while mas é um método
+//     console.log(`O número é ${numero}`)
+// })
+
+// const posts = [
+//     {title: "Primeiro post", category: "PHP"},
+//     {title: "Segundo post", category: "JavaScript"},
+//     {title: "Terceiro post", category: "Python"},
+// ]
+
+// posts.forEach((post) => {
+//     console.log(`Exibindo post: ${post.title}, da categoria: ${post.category}`) //tem que colocar a variavel.o tipo dela
+// })
+
+// // 16 - includes
+// const marcas = ["BMW", "VW", "Fiat"]
+
+// console.log(marcas.includes("Fiat")) //retorna um booleano 
+
+// if(marcas.includes("BMW")) { //uma forma de usar o includes
+//     console.log("Há carros da marca BMW!") 
+// }
+
+// // 17 - Reverse
+// //Ele inverte os elementos de um array
+
+// const reverseTest = [1, 2, 3, 4, 5]
+
+// reverseTest.reverse()
+
+// console.log(reverseTest)
+
+// 18 - Trim
+const trimTeste = "        testando\n   "
+
+console.log(trimTeste)
+
+console.log(trimTeste.trim())//Trim Remove tudo que não é texto em uma string(caracteres especiais e espaços em branco)
+
+// 19 - padStart
+//insere um texto no começo da string
+const testePadstart = "1"
+
+const novoNum  = testePadstart.padStart(4, "0") //onde está o 4 delimita o tamanho/depois da virgula escreve o que quer que apareca
+//o padstart colcoca para ser inserio antes da variavel
+console.log(testePadstart)
+
+console.log(novoNum)
+
+const testepadEnd = novoNum.padEnd(10, "0") //o padEnd coloca o que está em "" depois da variavel
+
+console.log(testepadEnd)
+
+// 20 - split
+//divide uma string em um array
+const frase = "Batatinha frita um dois três"
+
+const arrayFrase = frase.split(" ") //cada elemento é determinado por um separador em comum(Espaço,Virgula,ponto e virgula)
+
+console.log(arrayFrase)
+
+// 21 - join
+//une um array em uma string
+const fraseDenovo = arrayFrase.join(" ")
+
+console.log(fraseDenovo)
+
+const itensComprar = ["teclado","monitor","cabo"]
+
+const fraseCompra = `Precisamos comprar: ${itensComprar.join(", ")}` //assim colocando uma virgula e espaço depois de cada elemento
+
+console.log(fraseCompra)
+
+// 22 - reapeat
+const palavra = "testando "
+
+console.log(palavra.repeat(4)) //repete n vezes onde n é o valor dentro ()
+
+// 23 - rest operator
+const somaInfinita = (...arg) => { //... aceita qualquer tipo 
+    let total = 0
+
+    for(let i = 0; i < arg.length; i++){
+        total+= arg[i] //+= soma o número e substitui o total 
+    }
+    return total
 }
 
-console.log(carro)
+console.log(somaInfinita(1, 2, 3,))
 
-carro.porta = 4 // adicionado uma nova propriedade
-
-console.log(carro)
-
-delete carro.km // deletando uma propriedade
-
-console.log(carro)
-
-// 
-const obj = {
-    a: "teste",
-    b: true,
-}
-
-console.log(obj instanceof Object) //instanceof  verifica se um objeto é filho de uma classe
-
-const obj2 = {
-    c: [],
-}
-
-Object.assign(obj2, obj)
-
-console.log(obj2)
-
-// 8 - Conhecendo melhor os objetos
-console.log(Object.keys(obj)) //keys: verifica as propriedades de um objeto
-console.log(Object.keys(obj2))
-
-console.log(Object.entries(obj)) //entries: recebemos arrays dos nomes das propriedades com seus valores
-
-// 9 - mutação
-//quando criamos um objeto a partir de outro, este novo objeto não é novo e sim uma referência do primeiro
-const a = {
-    name: "lorenzo"
-}
-
-const b = a //adicionado a em b, b se torna uma referência de a e vice versa
-
-console.log(a) 
-console.log(b) //Adicionou o "lorenzo"
-
-a.ano = 19 //adicionando ano ao a
-
-console.log(a)
-console.log(b) //Vemos que também adicionou no b
-
-delete b.ano //deletando ano do b
-
-console.log(a) //vemos que também deletou do a
-console.log(b)
+console.log(somaInfinita(1, 2, 3, 4, 5, 6, 7))
