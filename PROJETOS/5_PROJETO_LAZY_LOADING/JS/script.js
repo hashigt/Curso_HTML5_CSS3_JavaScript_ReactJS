9845601a1e880e01e00c65005ca5bdb32bf90437
hashigt <129803254+hashigt@users.noreply.github.com>
2025-07-22 09:57:45 -0300

    atualização

--- PROJETOS/5_PROJETO_LAZY_LOADING/JS/script.js
+++ PROJETOS/5_PROJETO_LAZY_LOADING/JS/script.js
@@ -1,5 +1,17 @@
 const images = document.querySelectorAll(".image-container img");
 
+const observer = new IntersectionObserver((entries, observer) => { /*IntersectionObserver ele permite detectar quando um elemento se torna visível ou invisível na tela*/
+   entries.forEach(entrada => {
+      if(!entrada.isIntersecting) return
+
+      const image = entrada.target
+
+       image.src = image.src.replace("?w=10", "?w=1000");
+
+       observer.unobserve(image)
+   })
+}, {})
+
 images.forEach((image) => {
-   image.src = image.src.replace("?w=10", "?w=1000");
+   observer.observe(image)
 });
