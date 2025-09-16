706558e83bbf22fdfd837a030208540ad7d0e7a4
hashigt <129803254+hashigt@users.noreply.github.com>
2025-09-16 15:50:51 -0300

    adição do inicio do react(arquivos 1 e 2)

---
+++ REACT_JS/2_CONCEITOS_BASE/fundamentos/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+//importando o componente firstComponent do 
+import FirstComponent from './components/FirstComponent'
+
+function App() {
+  return (
+      <div className="app">
+        <h1>Fundamentos do React</h1>
+        <FirstComponent />
+      </div>
+  )
+}
+
+export default App;
