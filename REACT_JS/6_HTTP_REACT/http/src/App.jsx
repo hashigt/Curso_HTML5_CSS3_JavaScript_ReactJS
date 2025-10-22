7d94bc646aa36b31785ad59d752a33b01052476c
hashigt <129803254+hashigt@users.noreply.github.com>
2025-10-22 15:53:36 -0300

    react att

---
+++ REACT_JS/6_HTTP_REACT/http/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+const url = "http://localhost:3000/products"
+
+import './App.css'
+
+function App() {
+  // 1 - resgatando dados
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    
+  async function getData() {
+    const res = await fetch(url)
+
+    const data = await res.json();
+
+    setProducts(data)
+  }
+
+  getData();
+  }, [])
+
+  return (
+      <div className="card">
+        <h1>HTTP em react</h1>
+        {/* 1 - resgate de dados */}
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>{product.name} - R${product.price}</li>
+          ))}
+        </ul>
+      </div>
+     
+  )
+}
+
+export default App
